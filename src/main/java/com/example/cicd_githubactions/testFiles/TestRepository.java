e10df17ef87f27029fa3d3ce264d5add84aec941
littlezero48 <littlezero.048@gmail.com>
2023-01-25 17:22:21 +0900

    마지막 배포 성공 파일 때문에 발생한 오류일지도 몰라서 잠시 주석

--- src/main/java/com/example/cicd_githubactions/testFiles/TestRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.example.cicd_githubactions.testFiles;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface TestRepository extends JpaRepository<TestEntity, Long> {
-//}
